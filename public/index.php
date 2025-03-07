cd334d43fbd565bc0260050bdaed0223ad67b0c0
unknown <=>
2025-03-07 01:07:50 +0000

    ??????

--- public/index.php
+++ public/index.php
@@ -3,14 +3,17 @@
 require_once dirname(__DIR__)."/vendor/autoload.php";
 
 use app\core\Application;
-
+use app\src\controller\helloController;
 
 $app = new Application();
 
 // echo dirname(__DIR__)."./vendor/autoload.php";
 
 $app->router->get("/hello", "hello");
-$app->router->get("/amha", "amha");
+
+$app->router->get("/amha", [helloController::class,"index"]);
+
+$app->router->get("/", "amha");
 
 $app->run();
 
